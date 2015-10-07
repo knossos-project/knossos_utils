b5f702e9a146d253e6630d4cd94ad9bbfe30e400
my-tien <adtieni@gmail.com>
2015-10-07 19:42:48 +0200

    setup.py for creating a wheel and installing with pip

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+have_cython = False
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext as build_ext
+    have_cython = True
+except ImportError:
+    from distutils.command.build_ext import build_ext as build_ext
+from setuptools import setup
+from setuptools import Extension
+import numpy
+import os
+
+extensions = []
+if have_cython:
+    extensions.append(Extension("mergelist_tools",
+                                ['dataset_utils/mergelist_tools.pyx'],
+                                include_dirs = [numpy.get_include()],
+                                language="c++",
+                                extra_compile_args=["-std=c++0x", "-include", "cmath"]))
+else:
+    extensions.append(Extension('mergelist_tools',
+                                ['dataset_utils/mergelist_tools.cpp'],
+                                include_dirs = [numpy.get_include()],
+                                language="c++",
+                                extra_compile_args=["-std=c++0x", "-include", "cmath"]))
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup (
+    name = "knossos_python_tools",
+    version = "1.0",
+    description = "Tools for generating or manipulating knossos datasets and annotation files",
+    author = "Sven Dorkenwald, KNOSSOS team",
+    author_email = "knossos-team@mpimf-heidelberg.mpg.de",
+    url = "https://github.com/knossos-project/knossos_python_tools",
+    license = "GPL",
+    long_description = read("README.md"),
+    packages = ["dataset_utils"],
+    data_files = [("", ["LICENSE"])],
+    ext_modules = extensions,
+    cmdclass = {"build_ext": build_ext},
+    install_requires=[
+        "h5py",
+        "numpy",
+        "python-snappy",
+        "scipy",
+    ]
+)
