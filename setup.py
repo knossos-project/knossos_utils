bdbacd85e595579b5a52ab9752f3d712d2d0b3f9
mdraw <mdraw.gh@gmail.com>
2016-04-11 16:18:17 +0200

    Rewrite setup.py:
    - Cython is now automatically installed via setup_requires
    - Require Cython for building. Makes workarounds unnecessary
      and makes building more simple and reliable.
    - Explain that Numpy is required /before/ executing setup.py
    - Some style changes

--- setup.py
+++ setup.py
@@ -1,47 +1,43 @@
-#!/usr/bin/env python
-have_cython = False
-try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext as build_ext
-    have_cython = True
-except ImportError:
-    from distutils.command.build_ext import build_ext as build_ext
-from setuptools import setup
-from setuptools import Extension
-import numpy
-import os
+#!/usr/bin/env python2
 
-extensions = []
-if have_cython:
-    extensions.append(Extension("mergelist_tools",
-                                ['knossos_utils/mergelist_tools.pyx'],
-                                include_dirs = [numpy.get_include()],
-                                language="c++",
-                                extra_compile_args=["-std=c++0x", "-include", "cmath"]))
-else:
-    extensions.append(Extension('mergelist_tools',
-                                ['knossos_utils/mergelist_tools.cpp'],
-                                include_dirs = [numpy.get_include()],
-                                language="c++",
-                                extra_compile_args=["-std=c++0x", "-include", "cmath"]))
+from setuptools import setup, Extension
+import os
+try:
+    import numpy
+except ImportError:
+    print("Numpy not found. Please install Numpy manually: http://www.scipy.org/install.html")
+    raise SystemExit
+
+extensions = [Extension(
+    "mergelist_tools",
+    ["knossos_utils/mergelist_tools.pyx"],
+    include_dirs=[numpy.get_include()],
+    language="c++",
+    extra_compile_args=["-std=c++0x", "-include", "cmath"])
+]
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup (
-    name = "knossos_python_tools",
-    version = "1.0",
-    description = "Tools for generating or manipulating knossos datasets and annotation files",
-    author = "Sven Dorkenwald, KNOSSOS team",
-    author_email = "knossos-team@mpimf-heidelberg.mpg.de",
-    url = "https://github.com/knossos-project/knossos_python_tools",
-    license = "GPL",
-    long_description = read("README.md"),
-    packages = ["knossos_utils"],
-    data_files = [("", ["LICENSE"])],
-    ext_modules = extensions,
-    cmdclass = {"build_ext": build_ext},
+setup(
+    name="knossos_python_tools",
+    version="1.0",
+    description="Tools for generating or manipulating knossos datasets and annotation files",
+    author="Sven Dorkenwald, KNOSSOS team",
+    author_email="knossos-team@mpimf-heidelberg.mpg.de",
+    url="https://github.com/knossos-project/knossos_python_tools",
+    license="GPL",
+    long_description=read("README.md"),
+    packages=["knossos_utils"],
+    data_files=[("", ["LICENSE"])],
+    ext_modules=extensions,
+    setup_requires=[
+        "cython>=0.23",
+        "setuptools>=18.0",
+    ],
     install_requires=[
+        "cython",
         "h5py",
         "numpy",
         "python-snappy",
