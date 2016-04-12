256de505d4cefb48706387394e3b94bf3085417e
mdraw <mdraw.gh@gmail.com>
2016-04-12 18:16:44 +0200

    setup.py: Informative error messages
    ... for unsupported versions of Python and Setuptools
    
    Fix numpy-not-found error being swallowed by SystemExit

--- setup.py
+++ setup.py
@@ -2,11 +2,26 @@
 
 from setuptools import setup, Extension
 import os
+import sys
+
+if sys.version_info[:2] != (2, 7):
+    print('\nSorry, only Python 2.7 is currently supported.')
+    print('\nYour current Python version is {}'.format(sys.version))
+    sys.exit(1)
+
+# Setuptools >=18.0 is needed for Cython to work correctly.
+if parse_version(setuptools.__version__) < parse_version('18.0'):
+    print('\nYour installed Setuptools version is too old.')
+    print('Please upgrade it to at least 18.0, e.g. by running')
+    print('$ python2 -m pip install --upgrade setuptools')
+    print('If this fails, try additionally passing the "--user" switch to the install command, or use Anaconda2.')
+    sys.exit(1)
+
 try:
     import numpy
 except ImportError:
     print("Numpy not found. Please install Numpy manually: http://www.scipy.org/install.html")
-    raise SystemExit
+    sys.exit(1)
 
 extensions = [Extension(
     "mergelist_tools",
@@ -34,9 +49,8 @@ setup(
     ext_modules=extensions,
     setup_requires=[
         "cython>=0.23",
-        "setuptools>=18.0",
     ],
-    install_requires=[
+    install_requires=[  # TODO: Determine minimum requirement versions
         "cython",
         "h5py",
         "numpy",
