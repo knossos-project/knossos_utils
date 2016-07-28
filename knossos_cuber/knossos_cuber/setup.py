91422481e162fc34d91ea8cd21700f8565a0dd17
mdraw <mdraw.gh@gmail.com>
2016-07-28 14:51:53 +0200

    Replace many more built-ins with future versions
    i.e. filter, round, next, input,
         bytes, hex, oct, chr, int
    
    These are mostly unused and are just imported
    for increased safety against regressions.

--- knossos_cuber/knossos_cuber/setup.py
+++ knossos_cuber/knossos_cuber/setup.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 # builtins is either provided by Python 3 or by the "future" module for Python 2 (http://python-future.org/)
-from builtins import range, map  # TODO: Import all other necessary builtins after testing
+from builtins import range, map, filter, round, next, input, bytes, hex, oct, chr, int  # TODO: Import all other necessary builtins after testing
 from functools import reduce
 
 from distutils.core import setup
