81361ff72776e68a2f494b106e3be1c3a7355d77
sdorkenw <sven.dorkenwald@mpimf-heidelberg.mpg.de>
2016-10-06 22:23:02 +0200

    added function to get next neighbouring chunks (up to 3) of a chunk

--- knossos_utils/chunky.py
+++ knossos_utils/chunky.py
@@ -153,6 +153,25 @@ def _export_cset_to_tiff_stack_thread(args):
                               [:boundary[0], :boundary[1], dz])
 
 
+def _switch_array_entries(this_array, entries):
+    """
+    Switches to array entries
+
+    Parameters
+    ----------
+    this_array: np.array
+    entries: list of int
+
+    Returns
+    -------
+    this_array: np.array
+    """
+    entry_0 = this_array[entries[0]]
+    this_array[entries[0]] = this_array[entries[1]]
+    this_array[entries[1]] = entry_0
+    return this_array
+
+
 def save_dataset(chunk_dataset):
     with open(chunk_dataset.path_head_folder+"/chunk_dataset.pkl", 'wb') \
             as output:
@@ -173,8 +192,8 @@ def load_dataset(path_head_folder, update_paths=False):
             else:
                 rel_path = this_cd.chunk_dict[key].folder.split('/')[-1]
             this_cd.chunk_dict[key].folder = path_head_folder + '/' + rel_path + '/'
-        print "... finished. Saving."
-        save_dataset(this_cd)
+        print "... finished."
+        # save_dataset(this_cd)
 
     return this_cd
 
@@ -228,8 +247,8 @@ class ChunkDataset(object):
         Parameters:
         -----------
 
-        knossos_path: str
-            path to the datasetfolder - not .../mag !
+        knossos_dataset_object: knossos dataset
+            has to be initialized beforehand
         box_size: 3 sequence of int
             defines the size of the box without overlap
         box_coords: 3 sequence of int
@@ -366,6 +385,60 @@ class ChunkDataset(object):
                                         chunk_size, dtype=np.int) * chunk_size
             chunk_rep.append(self.coord_dict[tuple(chunk_coordinate)])
         return chunk_rep
+
+    def get_neighbouring_chunks(self, chunk, chunklist=None):
+        """ Returns the numbers of all neighbours
+
+        Parameters:
+        -----------
+        cset: chunkDataset object
+        chunk: chunk object
+
+        Returns:
+        --------
+        list of int
+            -1 if neighbour does not exist, else number
+             order: [< x, < y, < z, > x, > y, > z]
+
+        """
+        if chunklist is None:
+            chunklist = self.chunklist
+
+        coordinate = np.array(chunk.coordinates)
+        neighbours = []
+        for dim in range(3):
+            try:
+                this_coordinate = \
+                    coordinate - _switch_array_entries(
+                        np.array([chunk.size[dim], 0, 0]),
+                        [0, dim])
+                neighbour = self.coord_dict[tuple(this_coordinate)]
+                if not chunklist is None:
+                    if neighbour in chunklist:
+                        neighbours.append(neighbour)
+                    else:
+                        neighbours.append(-1)
+                else:
+                    neighbours.append(neighbour)
+            except:
+                neighbours.append(-1)
+
+        for dim in range(3):
+            try:
+                this_coordinate = \
+                    coordinate + _switch_array_entries(
+                        np.array([chunk.size[dim], 0, 0]), [0, dim])
+                neighbour = self.coord_dict[tuple(this_coordinate)]
+                if not chunklist is None:
+                    if neighbour in chunklist:
+                        neighbours.append(neighbour)
+                    else:
+                        neighbours.append(-1)
+                else:
+                    neighbours.append(neighbour)
+            except:
+                neighbours.append(-1)
+        return neighbours
 
     def from_chunky_to_matrix(self, size, offset, name, setnames,
                               dtype=np.uint32, outputpath=None,
