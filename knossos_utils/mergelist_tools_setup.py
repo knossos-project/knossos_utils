1f43755b142a1eb22a2521ba2485baa19db248c8
Fabian Svara <svara@neuro.mpg.de>
2016-11-19 22:05:51 +0100

    Add setup script to build mergelist_tools

---
+++ knossos_utils/mergelist_tools_setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy as np
+from distutils.extension import Extension
+
+#
+# Run this as python mergelist_tools_setup.py build_ext --inplace 
+# to generate mergelist_tools.so which can be imported by python.
+#
+
+extensions = [
+    Extension('mergelist_tools',
+        sources=['mergelist_tools.pyx'],
+        include_dirs = [np.get_include()],
+        language='c++',
+        extra_compile_args=["-std=c++11"],
+        extra_link_args=["-std=c++11"])
+]
+
+setup(
+    ext_modules = cythonize(extensions),
+    language='c++',
+)
