164d7fa6a36ca9a2b5d967e611787b50002b7c42
My-Tien Nguyen <my-tien.nguyen@ariadne.ai>
2018-05-07 17:20:17 +0200

    skeleton utils: nx graph to annotation: keep data and node id

--- knossos_utils/skeleton_utils.py
+++ knossos_utils/skeleton_utils.py
@@ -2007,7 +2007,8 @@ def nx_graph_to_annotation(G, scaling=None):
     for cur_n in G.nodes():
         x, y, z = cur_n.getCoordinate()
         cur_n_copy = SkeletonNode()
-        cur_n_copy.from_scratch(a, x, y, z)
+        cur_n_copy.from_scratch(a, x, y, z, ID=cur_n.ID)
+        cur_n_copy.data = cur_n.data
         new_node_mapping[cur_n] = cur_n_copy
         a.addNode(cur_n_copy)
 
