14bdfb01389a873d6f1005690bfc797063051b6e
My-Tien Nguyen <my-tien.nguyen@ariadne.ai>
2019-01-17 15:39:16 +0100

    Make string comparisons for version

--- general_utilities/versions.py
+++ general_utilities/versions.py
@@ -12,9 +12,9 @@ def compare_version(v1, v2):
     v2 = list(v2)
 
     if len(v1) > len(v2):
-        v2.extend([0] * (len(v1) - len(v2)))
+        v2.extend(["0"] * (len(v1) - len(v2)))
     elif len(v1) < len(v2):
-        v1.extend([0] * (len(v2) - len(v1)))
+        v1.extend(["0"] * (len(v2) - len(v1)))
 
     v1v2 = [(v1[x], v2[x]) for (x, y) in enumerate(v1)]
 
