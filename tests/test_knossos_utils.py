98289d7ce05104878f0b431afd7710b703e42f0a
Eric Jelli <eric.jelli@caesar.de>
2021-06-11 21:31:08 +0200

    assert and add test for mags=None with no existing mag folders

--- tests/test_knossos_utils.py
+++ tests/test_knossos_utils.py
@@ -5,6 +5,12 @@ from knossos_utils import KnossosDataset
 
 
 @pytest.mark.parametrize("boundary", [np.array([7,9,10]), (7,9,10), [7, 9, 10]])
-def test_KnossosDataset_initialize_without_conf(tmp_path, boundary):
+def test_KnossosDataset_initialize_without_conf__boundary(tmp_path, boundary):
     kd = KnossosDataset()
     kd.initialize_without_conf(str(tmp_path), boundary=boundary, scale=(1, 1, 1), experiment_name='test', mags=[1], verbose=True)
+
+
+def test_KnossosDataset_initialize_without_conf__mags(tmp_path):
+    kd = KnossosDataset()
+    with pytest.raises(AssertionError):
+        kd.initialize_without_conf(str(tmp_path), boundary=(7, 9, 10), scale=(1, 1, 1), experiment_name='test', mags=None, verbose=True)
