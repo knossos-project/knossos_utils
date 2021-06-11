2d7a24b5c407f21a083e2be3e54f72d2fb313ed0
Eric Jelli <eric.jelli@caesar.de>
2021-06-11 20:55:27 +0200

    failing test: boundary requires numpy array as input

--- tests/test_knossos_utils.py
+++ tests/test_knossos_utils.py
@@ -1,8 +1,12 @@
 from knossos_utils import KnossosDataset
 import pytest
 
-def test_KnossosDataset_initialize_without_conf(tmp_path):
+import numpy as np
+
+@pytest.mark.parametrize("boundary", [np.array([7,9,10]), (7,9,10), [7, 9, 10]])
+def test_KnossosDataset_initialize_without_conf(tmp_path, boundary):
   from knossos_utils import KnossosDataset
   import numpy as np
   kd = KnossosDataset()
-  kd.initialize_without_conf(str(tmp_path), boundary=np.array([7, 9, 10]), scale=(1, 1, 1), experiment_name='test', mags=[1], verbose=True)
+  kd.initialize_without_conf(str(tmp_path), boundary=boundary, scale=(1, 1, 1), experiment_name='test', mags=[1], verbose=True)
+
