541363d893c6f411b9df97d91054a9336383cec5
Eric Jelli <eric.jelli@caesar.de>
2021-06-11 21:08:33 +0200

    failing tests: manification default parameter does not work

--- tests/test_knossos_utils.py
+++ tests/test_knossos_utils.py
@@ -4,7 +4,8 @@ import pytest
 import numpy as np
 
 @pytest.mark.parametrize("boundary", [np.array([7,9,10]), (7,9,10), [7, 9, 10]])
-def test_KnossosDataset_initialize_without_conf(tmp_path, boundary):
+@pytest.mark.parametrize("mags", [None, [1]])
+def test_KnossosDataset_initialize_without_conf(tmp_path, boundary, mags):
   kd = KnossosDataset()
-  kd.initialize_without_conf(str(tmp_path), boundary=boundary, scale=(1, 1, 1), experiment_name='test', mags=[1], verbose=True)
+  kd.initialize_without_conf(str(tmp_path), boundary=boundary, scale=(1, 1, 1), experiment_name='test', mags=mags, verbose=True)
 
