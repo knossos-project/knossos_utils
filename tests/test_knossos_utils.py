7627aca7fd7079cea22ddeefd66a326d0b36e945
Eric Jelli <eric.jelli@caesar.de>
2021-06-11 21:05:37 +0200

    fixed self.boundary array bug

--- tests/test_knossos_utils.py
+++ tests/test_knossos_utils.py
@@ -5,8 +5,6 @@ import numpy as np
 
 @pytest.mark.parametrize("boundary", [np.array([7,9,10]), (7,9,10), [7, 9, 10]])
 def test_KnossosDataset_initialize_without_conf(tmp_path, boundary):
-  from knossos_utils import KnossosDataset
-  import numpy as np
   kd = KnossosDataset()
   kd.initialize_without_conf(str(tmp_path), boundary=boundary, scale=(1, 1, 1), experiment_name='test', mags=[1], verbose=True)
 
