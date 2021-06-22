96dbab5e573e027e94fb90acb40bd06462102bc3
Eric Jelli <eric.jelli@caesar.de>
2021-06-22 12:02:08 +0200

    Added test case for requested changes in #23

--- tests/test_knossos_utils.py
+++ tests/test_knossos_utils.py
@@ -14,3 +14,9 @@ def test_KnossosDataset_initialize_without_conf__mags(tmp_path):
     kd = KnossosDataset()
     with pytest.raises(AssertionError):
         kd.initialize_without_conf(str(tmp_path), boundary=(7, 9, 10), scale=(1, 1, 1), experiment_name='test', mags=None, verbose=True)
+
+
+def test_KnossosDataset_initalize_without_conf__mags_1_make_mag_folder_False(tmp_path):
+    kd = KnossosDataset()
+    with pytest.raises(AssertionError):
+        kd.initialize_without_conf(str(tmp_path), boundary=(7, 9, 19), scale=(1, 1, 1), experiment_name='test', mags=[1], make_mag_folders=False, verbose=True)
